cbf723eef3535286667219acfe8d2e890549bd4d
electricats <58383000+electricats@users.noreply.github.com>
2020-04-08 18:27:40 +0800

    Add files via upload

---
+++ judgetouch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class judgetouch : MonoBehaviour
+{
+    public GameObject ui;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ui.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            print("唉你注意点！你撞到我了！");
+            ui.SetActive(true);
+        }
+       
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        print("谢天谢地你走了！");
+        ui.SetActive(false);
+    }
+}
---
+++ move.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //speed = 1.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+
+        {
+
+        this.gameObject.transform.Translate(Vector3.forward*Time.deltaTime*speed);
+
+        }
+
+        if(Input.GetKey(KeyCode.S))
+
+        {
+
+        this.gameObject.transform.Translate(Vector3.back*Time.deltaTime*speed);
+
+        }
+
+        if(Input.GetKey(KeyCode.A))
+
+        {
+
+        this.gameObject.transform.Translate(Vector3.left*Time.deltaTime*speed);
+
+        }
+
+        if(Input.GetKey(KeyCode.D))
+
+        {
+
+        this.gameObject.transform.Translate(Vector3.right*Time.deltaTime*speed);
+
+        }
+        if(Input.GetKey(KeyCode.Space))
+
+        {
+
+        this.gameObject.transform.Translate(Vector3.up*Time.deltaTime*speed);
+
+        }
+        if (Input.GetKey(KeyCode.B))
+
+        {
+
+            this.gameObject.GetComponent<Renderer>().material.color = new Color(255f, 255f, 255f);
+
+        }
+        if (Input.GetKey(KeyCode.K))
+
+        {
+
+            this.gameObject.GetComponent<Renderer>().material.color = new Color(0f, 0f, 0f);
+            this.gameObject.GetComponent<Rigidbody>().mass = 10f;
+
+        }
+    }
+   
+}
